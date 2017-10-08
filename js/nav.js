d7a114db52b3cb297d0b8f0bd4e29f2ba017940f
Eric Chen <eric.chen@queensu.ca>
2017-10-08 15:35:06 -0400

    overlay covering everything
    fixed issue where overlay on the mobile nav menu was covering
    everything - used js to adjust the z-index

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 /* Open when someone clicks on the span element */
 function openNav() {
     document.getElementById("myNav").style.opacity = "1";
+    document.getElementById("myNav").style.zIndex = "1000";
     var navItems = document.getElementsByClassName('displayaz')
 	for (var i=0;i<navItems.length;i+=1) {
   		navItems[i].style.display = 'block';
@@ -11,6 +12,7 @@ function openNav() {
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.opacity = "0";
+    document.getElementById("myNav").style.zIndex = "-1";
     var navItems = document.getElementsByClassName('displayaz')
 	for (var i=0;i<navItems.length;i+=1) {
   		navItems[i].style.display = 'none';
